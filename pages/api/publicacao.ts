99e0a107c7c44c83a4d2e110181a72e4da1a1d5a
PauloScotti <paulo.scotti@hotmail.com>
2022-05-04 10:46:47 -0300

    Revert "Verificando se o problema é o cosmic"
    This reverts commit a3553ff28b5a73fd485b670378294f280e6dc1f9.

--- pages/api/publicacao.ts
+++ pages/api/publicacao.ts
@@ -29,6 +29,9 @@ const handler = nc()
             if (!descricao || descricao.length < 2) {
                 return res.status(400).json({ erro: 'Descição não é válida' });
             }
+            if (!req.file || !req.file.originalname) {
+                return res.status(400).json({ erro: 'Imagem é obrigatória' });
+            }
 
             const image = await uploadImagemCosmic(req);
             const publicação = {
